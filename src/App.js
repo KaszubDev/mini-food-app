ad63609381e07e5f98912d8004366ebb9659bb23
Krzysztof <krzysztof@mybusinessmatches.com>
2020-01-13 11:04:36 +0100

    navbar changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Container from '@material-ui/core/Container';
+import Sidebar from './components/Sidebar';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showSidebar: false,
+    };
+  }
+  changeSidebarVisibility = () => {
+    if(this.state.showSidebar === false){
+    this.setState({
+      showSidebar: true,
+    });
+    }
+    else {
+      this.setState({
+        showSidebar: false,
+      });
+    }
+  }
+  render() {
   return (
     <div>
+      <Sidebar visible={this.state.showSidebar}/>
       <Container maxWidth="lg">
-        <NavBar/>
+        <NavBar buttonOnclick={this.changeSidebarVisibility}/>
+        <Grid style={{marginTop: '100px',}} container spacing={3}>
+          <Grid item sm={4}>
+            <Paper>123</Paper>
+          </Grid>
+          <Grid item sm={4}>
+            <Paper>123</Paper>
+          </Grid>
+          <Grid item sm={4}>
+            <Paper>123</Paper>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, IconButton, InputBase } from '@material-ui/core';
+import { Toolbar, IconButton, InputBase, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 const Styles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
     },
+    bar: {
+        backgroundColor: '#FFFF8D',
+        color: '#000',
+    },
     search: {
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.25),
+        border: '1px solid #595959',
         '&:hover': {
             backgroundColor: fade(theme.palette.common.white,0.25),
         },
         marginLeft: 0,
-        width: '100%',
+        minHeight: '35px',
+        minWidth: "70%",
         [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
+            marginLeft: theme.spacing(20),
             width: 'auto',
         },
+    },
+    btn: {
+        marginLeft: theme.spacing(2),
+        minWidth: '12%',
+        borderRadius: theme.shape.borderRadius,
+        border: '1px solid #595959',
+        minHeight: '35px',
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white,0.25),
         },
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(8),
+        },
+    },
+    favoriteIcon: {
+        position: "absolute",
+        right: "20%",
+        [theme.breakpoints.up('sm')]: {
+            right: '15px',
+        },
+    },
     title: {
         flexGrow: 1,
         display: 'none',
@@ -43,7 +68,7 @@ const Styles = makeStyles(theme => ({
     inputInput: {
         padding: theme.spacing(1,1,1,7),
         transition: theme.transitions.create('width'),
-        width: '100%',
+        width: "100%",
         [theme.breakpoints.up('sm')]: {
             width: 120,
             '&:focus': {
@@ -52,38 +77,51 @@ const Styles = makeStyles(theme => ({
         }
     },
     inputRoot: {
+        width: "100%",
         color: 'inherit',
+    },
+    btnText: {
+        display: 'none',
+        margin: 0,
+        paddingRight: '20px',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        }
     },
     }));
 
-const NavBar = () => {
+const NavBar = (props) => {
+    const onclick = () => {
+        props.buttonOnclick();
+    };
     const classes = Styles();
-    return (
-        <div className={classes.root}>
-            <AppBar position='fixed'>
-                <Toolbar>
-                    <IconButton edge="start" color="inherit">
-                        <MenuIcon/>
-                    </IconButton>
-                <Typography className={classes.title} variant="h6" noWrap>
-                    Sample-App
-                </Typography>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon/>
+        return (
+            <div className={classes.root}>
+                <AppBar className={classes.bar} position='fixed'>
+                    <Toolbar>
+                        <IconButton className={classes.menuIcon} edge="start" color="inherit" onClick={onclick}>
+                            <MenuIcon/>
+                        </IconButton>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                    <InputBase placeholder="SEARCH"
+                    classes={{
+                        input:classes.inputInput,
+                        root: classes.inputRoot,
+                    }}
+                    inputProps={{'aria-label': 'search'}}
+                    />
                     </div>
-                <InputBase placeholder="Szukaj..."
-                classes={{
-                    input:classes.inputInput,
-                    root: classes.inputRoot,
-                }}
-                inputProps={{'aria-label': 'search'}}
-                />
-                </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-}
+                    <Button className={classes.btn}>
+                        <p className={classes.btnText}>FAVOURITES</p>
+                        <FavoriteBorderIcon className={classes.favoriteIcon}/>
+                    </Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    };
 
 export default NavBar;
